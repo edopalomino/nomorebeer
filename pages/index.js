74ca33473cbd6cf725b3b762ba48dc3b55697057
César E. Ortiz Palomino <otzeduardo@gmail.com>
2022-08-30 13:51:55 -0600

    Cambios menor

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import {connectToDatabase} from "./api/mongodb";
+import {connectToDatabase} from './api/mongodb';
 
 export default function Home({data}) {
 
